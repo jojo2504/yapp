6d75bca8f1a2eddae1e18f01426f92972fc09597
Jojo <jojo.duchuy@gmail.com>
2025-11-15 22:57:49 +0100

    fix(docker): no more graceful-stop timeout during container stops

--- judge/src/task/task.rs
+++ judge/src/task/task.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::{docker::DockerClient, models::{Output, Submission}};
 
 pub async fn process_submission(docker_client: &mut DockerClient, submission: &Submission) -> Result<Output, Box<dyn std::error::Error + Send + Sync>> {
@@ -6,33 +8,27 @@ pub async fn process_submission(docker_client: &mut DockerClient, submission: &S
     // Create the container for this specific submission
     if let Err(e) = docker_client.create_container(&container_name, &submission).await {
         eprintln!("create_container error: {:?}", e);
-        docker_client.delete_container(&container_name).await.ok();
+        docker_client.delete_container(&container_name, submission).await.ok();
         return Err(e);
     }
 
     // Start the container, and run all required command and tests
     docker_client.start_container(&container_name).await?;
 
-    println!("trying to build {}", &container_name);
-    match docker_client.build(&container_name, submission).await {
-        Ok(_) => (),
-        Err(e) => {
-            docker_client.delete_container(&container_name).await?;
-            panic!("deleting all containers and exiting during build: {}", e);
-        },
-    };
-    
-    println!("trying to run code {}", &container_name);
-    let output = match docker_client.run_command(&container_name, vec!["./main".to_string()], None).await {
+    // println!("trying to build {}", &container_name);
+    if submission.language.is_compiled() {
+        docker_client.build(&container_name, submission, Some(Duration::from_secs(2))).await?;
+    }
+    let output = match docker_client.run_command(&container_name, vec!["bash".to_string(), "-c".to_string(), submission.language.run_command()], None).await {
         Ok(output) => output,
         Err(e) => {
-            docker_client.delete_container(&container_name).await?;
+            docker_client.delete_container(&container_name, submission).await?;
             panic!("deleting all containers and exiting during code execution: {}", e);
         },
     };
-    
-    docker_client.delete_container(&container_name).await?;
-    println!("deleted container");
+
+    docker_client.delete_container(&container_name, submission).await?;
+    // println!("deleted container");
 
     Ok(output)
 }
